f8d33df68b76509846ed09f2df2e1525b6672781
Jacqueline Offei <offeijacqueline@gmail.com>
2025-02-24 13:40:00 +0000

    adding updates on products and hero sections

---
+++ script.js
@@ -0,0 +1,9 @@
+// let index = 0;
+// function slideShow() {
+//     let slides = document.querySelectorAll('.slides'); // Corrected selector
+//     slides.forEach(slide => slide.classList.remove('active')); 
+//     index = (index + 1) % slides.length;
+//     slides[index].classList.add('active'); 
+// }
+// setInterval(slideShow, 3000);
+
