a966b8a243e2988ac51f39870639ccdaf5e16809
Jacqueline Offei <offeijacqueline@gmail.com>
2025-03-10 19:37:52 +0000

    adding updates to storefront and contact form

--- script.js
+++ script.js
@@ -1,51 +1,29 @@
 const swiper = new Swiper(".mySwiper", {
-    spaceBetween: 20,
-    centeredSlides: true,
-    autoplay: {
-        delay: 7000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
-
-
 
 // testimonial slider
 
-var swiperr = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+// to open form
+function openForm() {
+  document.getElementById("contactForm").style.display = "flex";
+}
 
+// to close form
+function closeForm() {
+  document.getElementById("contactForm").style.display = "none";
+}
 
-
-  // to open form
-  function openForm() {
-    document.getElementById('contactForm').style.display='flex';
-  }
-
-  // to close form
-  function closeForm() {
-    document.getElementById("contactForm").style.display='none';
-  }
-
-
-
-  const container = document.querySelector(".testim-container");
+const container = document.querySelector(".testim-container");
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
 
@@ -73,3 +51,35 @@ function updateCarousel() {
   const cardWidth = document.querySelector(".card").offsetWidth;
   container.style.transform = `translateX(-${index * cardWidth}px)`;
 }
+
+// hamburger
+function toggleMenu() {
+  document.querySelector(".nav-links").classList.toggle("active");
+}
+
+// contact us form
+// Contact Us Form
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const form = this;
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+      method: "POST",
+      body: formData,
+      headers: { Accept: "application/json" },
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert("Thank you! Your message has been sent.");
+          form.reset();
+        } else {
+          alert("Yikes! Something went wrong.");
+        }
+      })
+      .catch(() => {
+        alert("Network error! Please try again later.");
+      });
+  });
